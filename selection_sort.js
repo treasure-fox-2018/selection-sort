257bcaa2468fc9bbdbe17446b605198f1ab64fee
Andre Sudi <y.andresudi@gmail.com>
2018-05-29 22:35:12 +0700

    selection sort yg bener

--- selection_sort.js
+++ selection_sort.js
@@ -1,23 +1,50 @@
-function sortManual(array) {
 
-    var temp = 0
+// INSERTION SORT
 
-    for (let i = 0; i < array.length; i++) {
-        for (let j = i+1; j < array.length; j++) {
-            // case 1 -> apakah 33 > 2? kalo iya jalanin yg bwh
-            if (array[i] > array[j]) {
-                // sekarang si temp nya isi si 33
-                temp = array[i]
-                // yg bawah utk nge swap jadi 2, 33
-                array[i] = array[j]
-                // j nya jadi 33
-                array[j] = temp
+// function sortManual(array) {
+
+//     var temp = 0
+
+//     for (let i = 0; i < array.length; i++) {
+//         for (let j = i+1; j < array.length; j++) {
+//             // case 1 -> apakah 33 > 2? kalo iya jalanin yg bwh
+//             if (array[i] > array[j]) {
+//                 // sekarang si temp nya isi si 33
+//                 temp = array[i]
+//                 // yg bawah utk nge swap jadi 2, 33
+//                 array[i] = array[j]
+//                 // j nya jadi 33
+//                 array[j] = temp
+//             }
+//         }
+//     }
+//     return array
+// }
+
+
+// console.log(sortManual([33, 2, 52, 106, 73])); //[2, 33, 52, 73, 106]
+// console.log(sortManual([13, 5, 22, 99, 11])); //[5, 11, 13, 22, 99]
+
+
+// SELECTION SORT
+
+function selectionSort(arr) {
+   
+    var emptyArr = []
+    
+    while(arr.length !== 0) {
+        var min = arr[0];
+        var tmp = 0
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i]
+                tmp = i
             }
         }
+        emptyArr.push(min)
+        arr.splice(tmp, 1)
     }
-    return array
+    return emptyArr
 }
-
-
-console.log(sortManual([33, 2, 52, 106, 73])); //[2, 33, 52, 73, 106]
-console.log(sortManual([13, 5, 22, 99, 11])); //[5, 11, 13, 22, 99]
+console.log(selectionSort([33, 2, 52, 106, 73])); //[2, 33, 52, 73, 106]
+console.log(selectionSort([13, 5, 22, 99, 11])); //[5, 11, 13, 22, 99]
